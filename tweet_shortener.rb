e91eaaaafc1b565af82e7cf198aa741986991e7e
Cody Yantis <cyantis@gmail.com>
2019-01-31 15:30:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,12 +30,12 @@ def bulk_tweet_shortener(string)
 end
 
 def selective_tweet_shortener(string)
-  string.length >= 140 ? word_substituter(string) : string
+  string.length > 140 ? word_substituter(string) : string
 end
 
 
 def shortened_tweet_truncator(string)
-  string.length >= 140 ? "#{string[0...136]}..." : string
+  string.length > 140 ? "#{string[0...137]}..." : string
 end
 
 #word_substituter("Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!")
