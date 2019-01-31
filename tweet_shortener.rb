8a848e9c34dbe24921604567e39ee2aa54337605
Cody Yantis <cyantis@gmail.com>
2019-01-31 15:26:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,8 +3,8 @@ require 'pry'
 def dictionary
   substitutes = {
   "hello" => "hi",
-  "to", "two", "too" => "2",
-  "for", "four" => "4",
+  "to, two, too" => "2",
+  "for, four" => "4",
   "be" => "b",
   "you" => "u",
   "at" => "@",
